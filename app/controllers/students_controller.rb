7da04e7d78183fe09ac4046c4e275415055fd8e0
nmhummel <natalie.hummel@gmail.com>
2020-12-17 12:47:30 -0600

    commit tree d1ab579ca99b619988998804e44312debc2af4d2

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,16 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    #@student = Show.new(params(:first_name, :last_name))
+    @student = Student.new
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    @student.save
+    redirect_to student_path(@student)
   end
 
 end
